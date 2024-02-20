2c91730eb63e11d374ae191477e03b55b6e2c097
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-02-20 21:56:08 +0530

    update

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 <?php
 session_start();
 session_destroy();
-echo "Logout successful";
+echo "<script>alert(Logout successful); window.location='index.php'</script>";
 header("location: index.php")
 ?>
