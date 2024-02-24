2a5e60cd8cb2e7429221ecc3d430556c00412e1b
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-02-24 09:25:08 +0530

    updated

--- ressubmit.php
+++ ressubmit.php
@@ -2,42 +2,43 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$servername = "opportunityjunction.mysql.database.azure.com";
-$username = $_ENV['MYSQL_USERNAME'];
-$password = $_ENV['MYSQL_PASSWORD'];
-$database="opportunity";
+require_once 'vendor/autoload.php';
 
-$conn = new mysqli($servername, $username, $password,$database);
+use Azure\Storage\Blob\BlobRestProxy;
+use Azure\Storage\Blob\Models\CreateBlobOptions;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=shubham;AccountKey=bPJNQN+VC6VndimOG7y1hKYZ6V5+m27U4q90V5CMCk01DbpQ8E9LDjgZ+yDjM/Auf+b6sV9PTGZE+ASt1lKigw==;EndpointSuffix=core.windows.net";
+$containerName = "shubham";
+
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
 $msg = "Submitted successfully";
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
+// Check if the form is submitted
 if (isset($_POST['submit'])) {
-    $targetDir = "uploads/";
-    $fileName = basename($_FILES["resume"]["name"]);
-    $targetFilePath = $targetDir . $fileName;
-    $fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+    $fileName = $_FILES["resume"]["name"];
+    $fileTmpName = $_FILES["resume"]["tmp_name"];
 
+    $targetFilePath = $fileName;
+
+    // Check file type
+    $fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
     $allowTypes = array('pdf', 'docx');
+
     if (in_array($fileType, $allowTypes)) {
-        if ($_FILES["resume"]["size"] > 5 * 1024 * 1024) {
-            $msg = "Sorry, your file is too large. It should be less than 5MB.";
-        } else {
-            if (move_uploaded_file($_FILES["resume"]["tmp_name"], $targetFilePath)) {
-                $sql = "INSERT INTO resumes (filename, date_uploaded) VALUES ('$fileName', NOW())";
-                if ($conn->query($sql) === true) {
-                    $msg = "Resume uploaded successfully.";
-                    header("Location:studash.php");
-                    exit();
-                } else {
-                    $msg = "Failed to upload resume: " . $conn->error;
-                }
-            } else {
-                $msg = "File upload failed.";
-            }
+        // Upload file to Azure Blob Storage
+        try {
+            $content = fopen($fileTmpName, "r");
+            $options = new CreateBlobOptions();
+            $options->setContentType(mime_content_type($fileTmpName));
+
+            $blobClient->createBlockBlob($containerName, $fileName, $content, $options);
+
+            $msg = "Resume uploaded successfully.";
+            header("Location:studash.php");
+            exit();
+        } catch (Exception $e) {
+            $msg = "Failed to upload resume: " . $e->getMessage();
         }
     } else {
         $msg = "Sorry, only PDF and DOCX files are allowed.";
