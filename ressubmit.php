77cde4f9ca0399cf312dcdc144540cbd01c6bb22
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-02-24 12:34:43 +0530

    updated

--- ressubmit.php
+++ ressubmit.php
@@ -17,26 +17,11 @@ if (isset($_POST['submit'])) {
 
     if (in_array($fileType, $allowTypes)) {
         // File type is allowed, echo success message
-        $msg = "File uploaded successfully.";
+        $msg = "<script>alert('Submitted Sucessfully'); window.location='studash.php'</script>.";
     } else {
         $msg = "Sorry, only PDF and DOCX files are allowed.";
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload Form</title>
-</head>
-<body>
-    <h2>Upload Resume</h2>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="resume" accept=".pdf,.docx">
-        <input type="submit" name="submit" value="Upload">
-    </form>
-    <?php echo $msg; ?> <!-- Display message -->
-</body>
-</html>
+
